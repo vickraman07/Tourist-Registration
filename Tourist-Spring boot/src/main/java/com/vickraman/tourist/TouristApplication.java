7ed9365d0648fd7b2031a88c21839bf4e4b27330
91996 <vickramankeerthan123@gmail.com>
2022-11-07 19:12:39 +0530

    second commit

--- Tourist-Spring boot/src/main/java/com/vickraman/tourist/TouristApplication.java
+++ Tourist-Spring boot/src/main/java/com/vickraman/tourist/TouristApplication.java
@@ -9,6 +9,7 @@ import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 public class TouristApplication {
 	@LoadBalanced
 	public static void main(String[] args) {
+		
 		SpringApplication.run(TouristApplication.class, args);
 	}
 
